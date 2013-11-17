cb5146dc05b28aeea9c2c106fe25b827ef2ddd08
Daan Louter <daan.louter@gmail.com>
2013-11-17 22:11:41 +0000

    first commit

---
+++ client/views/posts/sounds_list.js
@@ -0,0 +1,5 @@
+Template.soundsList.helpers({
+	sounds: function(){
+		return Sounds.find();
+	}
+})
---
+++ server/fixtures.js
@@ -0,0 +1,17 @@
+if (Sounds.find().count() === 0) { 
+	Sounds.insert({
+		title: 'Erol Alkan - Illumination EP (Previews)',
+		author: 'Phantasy Sound',
+		url: 'https://soundcloud.com/phantasysound/sets/erol-alkan-illumination-ep-preview'
+	});
+	Sounds.insert({
+		title: 'Moxie Minute Mix',
+		author: 'DISCOATTACK', 
+		url: 'https://soundcloud.com/bbcradio1/moxie-minute-mix'
+	});
+	Sounds.insert({
+		title: 'VPOO7 - Various Artists - Le Vibes And Pepper EP', 
+		author: 'Vibes And Pepper :: D3',
+		url: 'https://soundcloud.com/vapmanagement/vpoo7-various-artists-le-vibes'
+	}); 
+}
